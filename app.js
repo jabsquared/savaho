d5c78fb3635b8cc1ee3c1b02b3de42ce4c930a30
Louis Vichy <lgvichy@gmail.com>
2015-09-03 12:44:43 -0700

    added routing for Express

--- app.js
+++ app.js
@@ -21,9 +21,34 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+
+var mongo = services.mongolab[0].credentials;
+
+var uri = mongo.uri;
+
+var MongoClient = require('mongodb').MongoClient;
+
+var collection = null;
+
+MongoClient.connect(uri, function(err, db) {
+  if (err) {
+    return console.dir(err);
+  }
+
+  collection = db.collection('vote');
+});
+
+app.get('/api/:user', function(req, res) {
+  var ho_id = req.param.user;
+  collection.insert(ho_id);
+  res.send('Cool');
+});
+
+
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, function() {
-
-	// print a message when the server starts listening
+  // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
