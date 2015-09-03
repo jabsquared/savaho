286184bbc8a94e56f0693e7be5f8e8d656f05922
Yonas Berhe <sanoy@Yonass-MacBook-Air.local>
2015-09-03 11:05:28 -0700

    mvp

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var io = require('socket.io').listen(server);
 app.set('port', (process.env.PORT || 5000));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(__dirname, 'public/favicon'));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -36,6 +36,6 @@ app.post('/vote', routes.vote);
 
 io.sockets.on('connection', routes.vote);
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
